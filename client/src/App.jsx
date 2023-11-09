78c5084f993b39cb8ef51ef0714e597bdaf310bc
Nikifor Dragov <ndragov99@gmail.com>
2023-11-09 14:27:11 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import Header from "./components/Header"
-
+import Header from './components/Header';
 function App() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
+    const styles = {
+        backgroundImage: 'url("src/assets/cover1.jpg")',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '100vh',
+    };
+    return (
+        <div style={styles}>
+            <Header />
+        </div>
+    );
 }
 
-export default App
+export default App;
