80a801b167091dd1e977f1eb4f837c0593adad5b
Nikifor Dragov <ndragov99@gmail.com>
2023-11-09 15:10:13 +0200

    Add base card

---
+++ client/src/components/FighterCard.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CardGrid = () => {
+  const cardData = [
+    { title: 'Card 1', content: 'This is the content of Card 1' },
+    { title: 'Card 2', content: 'This is the content of Card 2' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+    { title: 'Card 3', content: 'This is the content of Card 3' },
+  ];
+
+  return (
+    <div className="container">
+      <div className="row">
+        {cardData.map((card, index) => (
+          <div key={index} className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{card.title}</h5>
+                <p className="card-text">{card.content}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardGrid;
