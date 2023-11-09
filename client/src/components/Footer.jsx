3499b3d4bd66432bc4de4ef942dbfa19ff4bc661
Nikifor Dragov <ndragov99@gmail.com>
2023-11-09 15:09:55 +0200

    Add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+        <footer className='bg-dark text-light py-3'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <p>&copy; Nikifor Dragov 2023</p>
+                    </div>
+                    <div className='col-md-6 text-md-right'>
+                        <a className='text-light' href='#top'>
+                            Back to Top
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
