f81d3ab2c14d567901c725172d419eea8af029a7
Nikifor Dragov <ndragov99@gmail.com>
2023-11-09 15:10:00 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
+import FighterCard from './components/FighterCard';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
     const styles = {
         backgroundImage: 'url("src/assets/cover1.jpg")',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        height: '100vh',
+        minHeight: '100vh',
     };
     return (
-        <div style={styles}>
+        <>
             <Header />
-        </div>
+            <div className='container-fluid d-flex flex-column' style={styles}>
+                <FighterCard />
+            </div>
+            <Footer />
+        </>
     );
 }
 
