bd1ec92bda9bcb41276f293d49511f29a0f59f90
Nikifor Dragov <ndragov99@gmail.com>
2023-11-12 21:00:23 +0200

    Create and style fighter cards

--- client/src/components/FighterCard.jsx
+++ client/src/components/FighterCard.jsx
@@ -1,51 +1,43 @@
 import React from 'react';
 
-const CardGrid = () => {
-  const cardData = [
-    { title: 'Card 1', content: 'This is the content of Card 1' },
-    { title: 'Card 2', content: 'This is the content of Card 2' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-    { title: 'Card 3', content: 'This is the content of Card 3' },
-  ];
+import { Card, Container, Button, Row, Col } from 'react-bootstrap';
+import './fighterCardStyles.css';
 
-  return (
-    <div className="container">
-      <div className="row">
-        {cardData.map((card, index) => (
-          <div key={index} className="col-md-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{card.title}</h5>
-                <p className="card-text">{card.content}</p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+const CardGrid = () => {
+    const cardData = [
+        { title: 'Card 1', content: 'This is the content of Card 1' },
+        { title: 'Card 2', content: 'This is the content of Card 2' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+        { title: 'Card 3', content: 'This is the content of Card 3' },
+    ];
+
+    return (
+        <Container>
+            <Row>
+                {cardData.map((card, index) => (
+                    <Col key={index} md={4}>
+                        <Card>
+                            <Card.Img
+                                variant='top'
+                                src='https://images.daznservices.com/di/library/DAZN_News/a3/49/conor-mcgregor_wx0eeamnxrs51rpr1b7n9o8gw.jpg?t=-1883069318&w=800'
+                            />
+                            <Card.Body>
+                                <Card.Title>{card.title}</Card.Title>
+                                <Card.Text>{card.content}</Card.Text>
+                                <Button variant='primary'>Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 };
 
 export default CardGrid;
