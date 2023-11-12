1573b6ef59d6160d0f5ed1383241b270803581ab
Nikifor Dragov <ndragov99@gmail.com>
2023-11-12 20:16:19 +0200

    Update navigation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,34 +5,29 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark" style={{marginBottom: '2em'}}>
-      <Container>
-        <Navbar.Brand href="#home">Fighters</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">All fighters</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">Sign In!</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Sign Up!
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Navbar
+            expand='lg'
+            className='bg-body-tertiary'
+            bg='dark'
+            data-bs-theme='dark'
+            style={{ marginBottom: '2em' }}
+        >
+            <Container>
+                <Navbar.Brand href='#home'>Fighters</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <Nav.Link href='#home'>Home</Nav.Link>
+                        <Nav.Link href='#link'>All fighters</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href='#deets'>Login</Nav.Link>
+                        <Nav.Link eventKey={2} href='#memes'>
+                            Register
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
