96a8b716f6d39dd2a84215f2ee8def371db958e1
Nikifor Dragov <ndragov99@gmail.com>
2023-11-13 18:28:50 +0200

    Add register component

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,11 @@
+export default function Register() {
+    return (
+        <>
+            <h1>Register Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Officiis, porro?
+            </p>
+        </>
+    );
+}
