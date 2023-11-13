4bccc63361fcb1144f0dd5d13263f9cd01e79d05
Nikifor Dragov <ndragov99@gmail.com>
2023-11-13 18:28:41 +0200

    Add login component

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+    return (
+        <>
+            <h1>Login Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Officiis, porro?
+            </p>
+        </>
+    );
+}
