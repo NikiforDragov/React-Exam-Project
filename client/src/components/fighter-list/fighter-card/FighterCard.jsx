4cee13b8b8b47ac1c200f4e1d595f6caee88d7af
Nikifor Dragov <ndragov99@gmail.com>
2023-11-26 21:00:28 +0200

    Update FighterCard.jsx

--- client/src/components/fighter-list/fighter-card/FighterCard.jsx
+++ client/src/components/fighter-list/fighter-card/FighterCard.jsx
@@ -1,14 +1,10 @@
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import AuthContext from '../../../contexts/authContext';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 
 export default function FighterCard({ _id, imageUrl, fighterName, age }) {
-    const { isAuthenticated } = useContext(AuthContext);
-
     return (
         <Col key={_id} md={4}>
             <Card className='custom-card'>
@@ -16,15 +12,13 @@ export default function FighterCard({ _id, imageUrl, fighterName, age }) {
                 <Card.Body>
                     <Card.Title>{`${fighterName}`}</Card.Title>
                     <Card.Text>{age}</Card.Text>
-                    {isAuthenticated && (
-                        <Button
-                            as={Link}
-                            to={`/fighters/${_id}/details`}
-                            variant='primary'
-                        >
-                            Details
-                        </Button>
-                    )}
+                    <Button
+                        as={Link}
+                        to={`/fighters/${_id}/details`}
+                        variant='primary'
+                    >
+                        Details
+                    </Button>
                 </Card.Body>
             </Card>
         </Col>
