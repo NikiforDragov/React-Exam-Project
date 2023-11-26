6f46b437262d63d18e1aff3ef3d84a8f46634f3f
Nikifor Dragov <ndragov99@gmail.com>
2023-11-26 19:37:14 +0200

    Update FighterDetails.jsx

--- client/src/components/fighter-details/FighterDetails.jsx
+++ client/src/components/fighter-details/FighterDetails.jsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+import * as fighterService from '../../services/fighterService';
+
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 export default function FighterDetails() {
-    return <h1 style={{ color: 'white' }}>Fighter Details</h1>;
+    const [fighter, setFighter] = useState({});
+    const { fighterId } = useParams();
+
+    useEffect(() => {
+        fighterService.getOne(fighterId).then((result) => {
+            console.log(result);
+            setFighter(result);
+        });
+    }, [fighterId]);
+
+    return (
+        <Card>
+            <Card.Img variant='top' src={fighter.imageUrl} />
+            <Card.Body>
+                <Card.Title>{fighter.fighterName}</Card.Title>
+                <Card.Text>{fighter.description}</Card.Text>
+            </Card.Body>
+            <ListGroup className='list-group-flush'>
+                <ListGroup.Item>{fighter.age}</ListGroup.Item>
+                <ListGroup.Item>{fighter.fightingStyle}</ListGroup.Item>
+                <ListGroup.Item>{fighter.category}</ListGroup.Item>
+                <ListGroup.Item>{fighter.country}</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link as={Link} to='/fighters'>
+                    Back
+                </Card.Link>
+                <Card.Link as={Link} to='/fighters/edit'>
+                    Edit
+                </Card.Link>
+                <Card.Link as={Link} to='/fighters/delete'>
+                    Delete
+                </Card.Link>
+            </Card.Body>
+        </Card>
+    );
 }
+
