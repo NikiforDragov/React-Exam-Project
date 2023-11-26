74ee36ab1e39f0086d07d5a1ddb3d24874947919
Nikifor Dragov <ndragov99@gmail.com>
2023-11-26 16:57:01 +0200

    Add controlled login form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
@@ -6,23 +7,57 @@ import Container from 'react-bootstrap/Container';
 
 import styles from './Login.module.css';
 
+const formInitialState = {
+    email: '',
+    password: '',
+};
+
 export default function Login() {
+    const [loginValues, setLoginValues] = useState(formInitialState);
+
+    const changeHandler = (e) => {
+        setLoginValues((state) => ({
+            ...state,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const loginSubmitHandler = (e) => {
+        e.preventDefault();
+
+        console.log(loginValues);
+    };
+
     return (
         <Container className={styles.formContainer}>
-            <Form className={styles.form}>
-                <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form className={styles.form} onSubmit={loginSubmitHandler}>
+                <Form.Group className='mb-3' controlId='formEmail'>
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type='email' placeholder='Enter email' />
+                    <Form.Control
+                        type='email'
+                        name='email'
+                        value={loginValues.email}
+                        onChange={changeHandler}
+                        placeholder='Enter email'
+                    />
                 </Form.Group>
-                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                <Form.Group className='mb-3' controlId='formPassword'>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type='password' placeholder='Password' />
+                    <Form.Control
+                        type='password'
+                        name='password'
+                        value={loginValues.password}
+                        onChange={changeHandler}
+                        placeholder='Password'
+                    />
                 </Form.Group>
                 <div className={styles.buttonContainer}>
                     <Button variant='dark' type='submit'>
                         Login
                     </Button>
-                    <Link className={styles.link} to='/users/register'>Don't have an account?</Link>
+                    <Link className={styles.link} to='/users/register'>
+                        Don't have an account?
+                    </Link>
                 </div>
             </Form>
         </Container>
