2819caa8bfbea3ad3c740922d7d9d6783262f644
Nikifor Dragov <ndragov99@gmail.com>
2023-12-01 19:03:13 +0200

    Add edit fighter path

--- client/src/constants/paths.js
+++ client/src/constants/paths.js
@@ -2,6 +2,7 @@ export const Path = {
     HOME: '/',
     ALL_FIGHTERS: '/fighters',
     CREATE_FIGHTER: '/fighters/create',
+    EDIT_FIGHTER: '/fighters/:fighterId/edit',
     FIGHTER_DETAILS: '/fighters/:fighterId/details',
     LOGIN: '/users/login',
     REGISTER: '/users/register',
