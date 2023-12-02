e1ef8c401baa3abcf9edb6df557d5d062752b464
Nikifor Dragov <ndragov99@gmail.com>
2023-12-02 21:26:58 +0200

    Add delete path

--- client/src/constants/paths.js
+++ client/src/constants/paths.js
@@ -1,10 +1,11 @@
 export const Path = {
     HOME: '/',
-    ALL_FIGHTERS: '/fighters',
-    CREATE_FIGHTER: '/fighters/create',
-    EDIT_FIGHTER: '/fighters/:fighterId/edit',
-    FIGHTER_DETAILS: '/fighters/:fighterId/details',
     LOGIN: '/users/login',
     REGISTER: '/users/register',
     LOGOUT: '/users/logout',
-}
+    ALL_FIGHTERS: '/fighters',
+    CREATE_FIGHTER: '/fighters/create',
+    EDIT_FIGHTER: '/fighters/:fighterId/edit',
+    DELETE_FIGHTER: '/fighters/:fighterId/delete',
+    FIGHTER_DETAILS: '/fighters/:fighterId/details',
+};
