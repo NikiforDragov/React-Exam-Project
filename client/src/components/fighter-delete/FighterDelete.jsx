cc1cbf5c05c97dd8cd46529c8808aeda064ce25d
Nikifor Dragov <ndragov99@gmail.com>
2023-12-02 21:58:06 +0200

    Implement delete fighter

--- client/src/components/fighter-delete/FighterDelete.jsx
+++ client/src/components/fighter-delete/FighterDelete.jsx
@@ -1,18 +1,41 @@
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import * as fighterService from '../../services/fighterService';
+import { Path } from '../../constants/paths';
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
 export default function FighterDelete() {
+    const { fighterId } = useParams();
+    const [ fighter, setFighter ] = useState({});
     const navigate = useNavigate();
+
+    useEffect(() => {
+        fighterService
+            .getOne(fighterId)
+            .then((result) => setFighter(result))
+            .catch((error) => console.error('Error fetching fighter:', error));
+    }, [fighterId]);
+
+    const deleteFighterHandler = async () => {
+        try {
+            await fighterService.del(fighterId);
+            navigate(Path.ALL_FIGHTERS);
+        } catch (error) {
+            console.error('Error deleting fighter:', error);
+        }
+    };
+
     return (
         <Container>
             <h2>Delete Fighter</h2>
-            <p>Are you sure you want to delete {fighterData.fighterName}?</p>
-            <Button variant='danger' onClick={handleDelete}>
+            <p>Are you sure you want to delete {fighter.fighterName}?</p>
+            <Button variant='danger' onClick={deleteFighterHandler}>
                 Delete
-            </Button>{' '}
-            <Button variant='secondary' onClick={() => navigate(Path.FIGHTERS)}>
+            </Button>
+            <Button variant='secondary' onClick={() => navigate(Path.ALL_FIGHTERS)}>
                 Cancel
             </Button>
         </Container>
