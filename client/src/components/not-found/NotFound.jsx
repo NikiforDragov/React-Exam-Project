8b6da244c77bc03fafddf00f166cc89c812cd9b3
Nikifor Dragov <ndragov99@gmail.com>
2023-12-03 16:50:19 +0200

    Style 404 page

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -1,9 +1,34 @@
 import { Link } from 'react-router-dom';
 import { Path } from '../../constants/paths';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/esm/Button';
 
 export default function NotFound() {
     return (
-        <h1>404</h1>
+        <Container>
+            <Row
+                className='justify-content-center align-items-center'
+                style={{ height: '100vh', backgroundColor: 'white' }}
+            >
+                <Col xs={12} md={6} className='text-center'>
+                    <Image
+                        src='/public/404.png'
+                        alt='404 Not Found'
+                        fluid
+                    />
+                </Col>
+                <Col xs={12} md={6} className='text-center'>
+                    <h1>404 Not Found</h1>
+                    <p>
+                        The page you are looking for might be in another galaxy.
+                    </p>
+                    <Button variant={'dark'} as={Link} to='/'>Go Home</Button>
+                </Col>
+            </Row>
+        </Container>
     );
 }
