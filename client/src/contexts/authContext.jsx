b43059242f773fb6e946ef2b70719ce1a2f2e9a2
Nikifor Dragov <ndragov99@gmail.com>
2023-12-04 19:12:02 +0200

    Add error handling

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -13,23 +13,25 @@ export const AuthProvider = ({ children }) => {
     });
 
     const loginSubmitHandler = async ({ email, password }) => {
-        const result = await authService.login(email, password);
-
-        setAuth(result);
-
-        localStorage.setItem('accessToken', result.accessToken);
-
-        navigate('/');
+        try {
+            const result = await authService.login(email, password);
+            setAuth(result);
+            localStorage.setItem('accessToken', result.accessToken);
+            navigate('/');
+        } catch (error) {
+            console.error('Login failed:', error);
+        }
     };
 
     const registerSubmitHandler = async ({ email, password }) => {
-        const result = await authService.register(email, password);
-
-        setAuth(result);
-
-        localStorage.setItem('accessToken', result.accessToken);
-
-        navigate('/');
+        try {
+            const result = await authService.register(email, password);
+            setAuth(result);
+            localStorage.setItem('accessToken', result.accessToken);
+            navigate('/');
+        } catch (error) {
+            console.error('Register failed:', error);
+        }
     };
 
     const logoutHandler = () => {
