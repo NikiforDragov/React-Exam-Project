4443c9502bc0e09dab8ed27e4b1d1ef46e64480f
Nikifor Dragov <ndragov99@gmail.com>
2023-12-07 19:11:33 +0200

    Add error handling

--- client/src/components/fighter-search/FighterSearch.jsx
+++ client/src/components/fighter-search/FighterSearch.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 
+import ErrorAlert from '../error-alert/ErrorAlert';
+
 import useForm from '../../hooks/useForm';
 import * as fighterService from '../../services/fighterService';
 
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/esm/Button';
+import Alert from 'react-bootstrap/Alert';
 
 import styles from './FighterSearch.module.css';
 import FighterList from '../fighter-list/FighterList';
@@ -15,7 +18,9 @@ export default function Search() {
         document.title = 'Search';
     }, []);
 
+    const [serverError, setServerError] = useState(null);
     const [searchedFighter, setSearchedFighter] = useState([]);
+    const [isSearched, setIsSearched] = useState(false);
 
     const searchValidate = (values) => {
         const errors = {};
@@ -28,15 +33,19 @@ export default function Search() {
     };
 
     const searchHandler = async () => {
-        if (values.fighterName !== '') {
+        try {
             const result = await fighterService.searchByName(
                 values.fighterName
             );
             setSearchedFighter(result);
+            setIsSearched(true);
+        } catch (error) {
+            console.error(error.message);
+            setServerError(error.message);
         }
     };
 
-    const {values, formErrors, touched, onChange, onBlur, onSubmit} = useForm(
+    const { values, formErrors, touched, onChange, onBlur, onSubmit } = useForm(
         searchHandler,
         {
             fighterName: '',
@@ -56,14 +65,37 @@ export default function Search() {
                         value={values.fighterName}
                         onChange={onChange}
                         onBlur={onBlur}
-                        isInvalid={touched.fighterName && !!formErrors.fighterName}
+                        isInvalid={
+                            touched.fighterName && !!formErrors.fighterName
+                        }
                     />
                     <Button type='submit' variant='secondary'>
                         Search
                     </Button>
                 </Form>
+                <Container>
+                    {serverError && (
+                        <ErrorAlert
+                            ErrorMessage={serverError}
+                            onClose={() => setServerError(null)}
+                        />
+                    )}
+                </Container>
             </Container>
-            <FighterList fighters={searchedFighter} />
+            {searchedFighter.length > 0 && (
+                <FighterList fighters={searchedFighter} />
+            )}
+            {isSearched && searchedFighter.length === 0 && (
+                <Container className={styles.noResults}>
+                    <Alert
+                        variant='danger'
+                        onClick={() => setIsSearched(false)}
+                        dismissible
+                    >
+                        <p>Sorry no results!</p>
+                    </Alert>
+                </Container>
+            )}
         </>
     );
 }
